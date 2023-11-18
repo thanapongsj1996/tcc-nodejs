335181676e3df4939075b5e27a7d05a02b087091
Boy Thanapong <thanapongsj1996@gmail.com>
2023-11-18 15:14:04 +0700

    add k8s

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
+
 const PORT = 3000;
+const envName = process.env.ENV_NAME || "not found";
 
 // Define a route that responds with "Hello, World!"
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World! --> " + envName);
 });
 
 // Start the Express server
